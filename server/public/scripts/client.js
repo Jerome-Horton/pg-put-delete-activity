e839fb2f4a8027a86a0acc4f9b161f46cff96e4d
Jerome Horton <jeromehorton1@gmail.com>
2021-11-17 21:25:10 -0600

    created functions for Delete & MArk As Read

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
 
 function addClickHandlers() {
   $('#submitBtn').on('click', handleSubmit);
+  $('#bookShelf').on('click', 'deleteBtn', deleteBooks);
+  $('#submitBtn').on('click', booksRead);
+
+  // Add  buttons and functions in here
 
   // TODO - Add code for edit & delete buttons
 }
@@ -58,7 +62,39 @@ function renderBooks(books) {
       <tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
+        <td>${book.isRead}</td>
+        <td><button class="deleteBtn" data-id="${book.id}">Delete</button></td>
+        <td><button class=isReadBtn" data-id="${book.id}">Mark As Read</button></td>
       </tr>
     `);
+    // Line 67, need to pass button data for req.body of the "PUT"
   }
+}
+
+function deleteBooks() {
+  const booksIdToDelete = $(this).data('id');
+  console.log('bookid = ');
+  $.ajax({
+    type: 'DELETE',
+    url: `/books/${booksIdToDelete}`
+  }).then((response) => {
+    console.log(response);
+    refreshBooks();
+  })
+};
+
+function booksRead() {
+  const booksIdToMark = $(this).data('id');
+
+  console.log('bookIdToMark', booksIdToMark);
+
+  $.ajax({
+    type: 'PUT',
+    url: `/books/${booksIdToUpvote}`,
+    data: {  } // needs data object
+  }).then((res) => {
+    refreshBooks();
+  }).catch((err) => {
+    console.error(err);
+  })
 }
